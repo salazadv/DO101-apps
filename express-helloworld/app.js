c95dd01de0369f4089836e71af15afa38cd47510
Ricardo Salazar <salazar.adv@gmail.com>
2022-08-02 01:48:40 -0300

    add /october endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -2,7 +2,11 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello World! Hello Brazil! Hello Brasilia - DF!\n');
+});
+
+app.get('/october', function(req, res) {
+  res.send('Welcome October! Welcome R I C H A R D !');
 });
 
 app.listen(8080, function () {
